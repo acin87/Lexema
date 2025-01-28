b27f90aeec64220ab1a383b8c275e8022b3dd07d
acin87 <vadimzizi@mail.ru>
2025-01-28 21:09:55 +0500

    Update new Comments

--- src/components/Comments/CommentsV2/Comment.tsx
+++ src/components/Comments/CommentsV2/Comment.tsx
@@ -1,9 +1,25 @@
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, Divider, IconButton, Typography } from '@mui/material';
-import { FC, Fragment } from 'react';
+import {
+    Avatar,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    Divider,
+    IconButton,
+    InputAdornment,
+    Stack,
+    TextField,
+    Tooltip,
+    Typography,
+} from '@mui/material';
+import { FC, Fragment, useState } from 'react';
 import { CommentType } from '../../../app/reducers/comments/commntsType';
 import { useGetUserByIdQuery } from '../../../app/reducers/user/userApi';
 import { formatTimeAgo } from '../../../app/util/Utils';
+import CommentIcon from '@mui/icons-material/Comment';
+import ReplyIcon from '@mui/icons-material/Reply';
 
 type CommentProps = {
     comment: CommentType;
@@ -11,6 +27,12 @@ type CommentProps = {
 
 const Comment: FC<CommentProps> = ({ comment }) => {
     const { data: user } = useGetUserByIdQuery({ id: comment.user.id });
+    const [expandArea, setExpandArea] = useState(false);
+
+    const answerBtn = () => {
+        setExpandArea(!expandArea);
+    };
+
     return (
         <Fragment>
             <Card sx={{ boxShadow: 'none', width: '100%', paddingBottom: 0 }}>
@@ -18,9 +40,19 @@ const Comment: FC<CommentProps> = ({ comment }) => {
                     sx={{ paddingBottom: 0 }}
                     avatar={<Avatar src={user?.image} />}
                     action={
-                        <IconButton aria-label="settings">
-                            <MoreVertOutlinedIcon />
-                        </IconButton>
+                        <Fragment>
+                            <Stack direction="row">
+                                <Tooltip title="репост" placement="top">
+                                    <IconButton>
+                                        <ReplyIcon></ReplyIcon>
+                                    </IconButton>
+                                </Tooltip>
+
+                                <IconButton aria-label="settings">
+                                    <MoreVertOutlinedIcon />
+                                </IconButton>
+                            </Stack>
+                        </Fragment>
                     }
                     title={
                         <Typography variant="body2" component="span">
@@ -39,9 +71,29 @@ const Comment: FC<CommentProps> = ({ comment }) => {
                     </Typography>
                 </CardContent>
                 <CardActions sx={{ p: 0 }} disableSpacing>
-                    <Button sx={{ textTransform: 'lowercase', ml: '16px' }} size="small">
-                        ответить
-                    </Button>
+                    {!expandArea ? (
+                        <Button sx={{ textTransform: 'lowercase', ml: '16px' }} size="small" onClick={answerBtn}>
+                            ответить
+                        </Button>
+                    ) : (
+                        <TextField
+                            placeholder="Ваш ответ"
+                            name="aswer-comments"
+                            variant="outlined"
+                            size="small"
+                            sx={{ width: '100%', p: 2 }}
+                            onBlur={() => setExpandArea(false)}
+                            slotProps={{
+                                input: {
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <CommentIcon />
+                                        </InputAdornment>
+                                    ),
+                                },
+                            }}
+                        />
+                    )}
                 </CardActions>
             </Card>
             <Divider variant="middle" />
